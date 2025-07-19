2f8c28c4ae4f4da0e030bf25b02d3c4ae319ae47
Mobarak Hosen Mohen <chemistmubarak1@gmail.com>
2025-07-19 23:28:50 +0600

    data type

--- main.py
+++ main.py
@@ -11,6 +11,31 @@ print("Hellow world")
 
 #Module & Pip intro
 #pythone variables
-name ="eshan"
+name ="Mohen"
 print(name)
 print(100)
+
+#pythone data type
+#string type
+hablu= 'kutta'
+mama = 'tui'
+print(hablu)
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+a = "Hello, World!"
+print(a[1])
+print(type(hablu))
+print(mama +' '+ hablu)
+print('my name is' +' '+ hablu)
+
+#number type
+x = 20
+y= 20.5
+z=20j
+print(z)
+print(x)
+print(y)
+print(type(x))
+print(type(y))
+print(type(z))
+
