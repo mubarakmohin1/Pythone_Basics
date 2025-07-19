7e673b3f122f9dab0acb4d9d546eeb4b0fe251f6
Mobarak Hosen Mohen <chemistmubarak1@gmail.com>
2025-07-19 10:40:04 +0600

    pytone comment

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from curses.ascii import controlnames
-
 
 print("there is a right way to learn to speak english")
+
+#pythone comment(first ine comment)
+
+''' multiline comment
+second line
+third line'''
+
+print("Hellow world")
